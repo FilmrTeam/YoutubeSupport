7f09d03558cba16d16a79eb48625338f16279ec0
Bernardo Alecrim <bernardo@filmrapp.com>
2020-02-20 15:00:52 -0300

    Initial commit

---
+++ YoutubeSupport/YoutubeSupport.h
@@ -0,0 +1,19 @@
+//
+//  YoutubeSupport.h
+//  YoutubeSupport
+//
+//  Created by Bernardo Alecrim on 20/02/20.
+//  Copyright © 2020 Filmr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YoutubeSupport.
+FOUNDATION_EXPORT double YoutubeSupportVersionNumber;
+
+//! Project version string for YoutubeSupport.
+FOUNDATION_EXPORT const unsigned char YoutubeSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoutubeSupport/PublicHeader.h>
+
+
